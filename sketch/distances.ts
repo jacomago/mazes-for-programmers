ddafe392387b050751f0f91944c59524b49dab24
Sky Brewer <jaco.brewer@gmail.com>
2022-07-27 19:08:52 +0200

    start path to

--- sketch/distances.ts
+++ sketch/distances.ts
@@ -21,4 +21,20 @@ class Distances {
         return this.cells.keys();
     }
 
+    path_to(goal: Cell): Distances {
+        let current = goal;
+
+        let breadcrumbs = new Distances(this.root);
+        breadcrumbs.set(current, this.cells.get(current));
+
+        while (current != this.root) {
+            for (let link of current.link_keys()) {
+                if (this.cells.get(current) > this.cells.get(link)) {
+                    current = link;
+                    break;
+                }
+            }
+        }
+        return breadcrumbs;
+    }
 }
