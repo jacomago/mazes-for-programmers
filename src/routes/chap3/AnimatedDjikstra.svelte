88a762eb41d3775c1e906867de97f15ca2cfefe9
Sky Brewer <jaco.brewer@gmail.com>
2022-07-29 17:59:09 +0200

    add to other page

--- src/routes/chap3/AnimatedDjikstra.svelte
+++ src/routes/chap3/AnimatedDjikstra.svelte
@@ -100,7 +100,7 @@
 		};
 	};
 </script>
-
+<h2>Animated djikstra</h2>
 <label>
 	Grid Size
 	<input type="range" bind:value={grid_size} min="4" max="100" step="1" />
--- src/routes/chap3/index.svelte
+++ src/routes/chap3/index.svelte
@@ -1,9 +1,12 @@
 <script>
+	import AnimatedDjikstra from './AnimatedDjikstra.svelte';
+
 	import Colored from './Colored.svelte';
 	import Djikstra from './Djikstra.svelte';
 	import LongestPath from './LongestPath.svelte';
 </script>
 
+<AnimatedDjikstra />
 <Colored />
 <LongestPath />
 <Djikstra />
