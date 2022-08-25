63e1d3c9a573109a9c2221d86af6e9859c27d197
Sky Brewer <jaco.brewer@gmail.com>
2022-08-25 15:18:57 +0200

    add backtracker to chap 5

--- src/routes/chap5/index.svelte
+++ src/routes/chap5/index.svelte
@@ -1,8 +1,12 @@
 <script>
+	import Backtracker from './backtracker.svelte';
+
 	import Compare from './compare.svelte';
 
 	import Huntkill from './huntkill.svelte';
 </script>
+
+<Backtracker />
 
 <Huntkill />
 <Compare />
