80948fe2a4edae7e2acf88d0a7822a66cf11d475
Sky Brewer <jaco.brewer@gmail.com>
2022-08-25 11:24:29 +0200

    format

--- src/lib/bin/backtracker.ts
+++ src/lib/bin/backtracker.ts
@@ -7,7 +7,7 @@ export function backtrack_colored_grid(
 	grid_size: number,
 	weights: Map<Direction, number>,
 	origin_x = Math.floor(grid_size / 2),
-	origin_y = Math.floor(grid_size / 2),
+	origin_y = Math.floor(grid_size / 2)
 ): DistanceGrid {
 	const grid = new ColoredGrid(grid_size, grid_size);
 	const start = grid.get(origin_x, origin_y);
