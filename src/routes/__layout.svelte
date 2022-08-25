57f03eb6a5ea97366e3459837e29110fb3e83be5
Sky Brewer <jaco.brewer@gmail.com>
2022-08-25 15:18:47 +0200

    add an about

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,7 @@
 	<a href="/chap3">Chap3</a>
 	<a href="/chap4">Chap4</a>
 	<a href="/chap5">Chap5</a>
+	<a href="/about">About</a>
 </nav>
 
 <slot />
---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<h1>About</h1>
+
+<p>
+	My collection of examples from the book<a href="http://www.mazesforprogrammers.com/"
+		>Mazes for Programmers</a
+	>.
+</p>
+
+<p>Code found on <a href="https://github.com/jacomago/mazes-for-programmers">github</a></p>
